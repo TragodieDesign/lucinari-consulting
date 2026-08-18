ba9261649e075521996357d8b6a986816201fc42
[dyad] <brunofernando@tragodiedesign.com.br>
2026-08-18 04:25:11 -0300

    chore: auto-commit local changes before connecting to GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import LeadChat from "@/components/LeadChat";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Footer, Header } from "@/components/SiteShell";
 import { ArchivePage, ArticlePage, GlossaryPage, GlossaryTermPage } from "./pages/ContentPages";
-
+import AboutPage from "./pages/AboutPage";
 import DetailPage from "./pages/DetailPage";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => <QueryClientProvider client={queryClient}><TooltipProvider><Toaster /><Sonner /><BrowserRouter><ScrollToTop /><div className="flex min-h-screen flex-col"><Header /><div className="flex-1"><Routes><Route path="/" element={<Index />} /><Route path="/solucoes/:slug" element={<DetailPage />} /><Route path="/treinamentos/:slug" element={<DetailPage />} /><Route path="/blog" element={<ArchivePage kind="blog" />} /><Route path="/conhecimentos" element={<ArchivePage kind="conhecimento" />} /><Route path="/cases" element={<ArchivePage kind="case" />} /><Route path="/glossario" element={<GlossaryPage />} /><Route path="/glossario/:slug" element={<GlossaryTermPage />} /><Route path="/:kind/:slug" element={<ArticlePage />} /><Route path="*" element={<NotFound />} /></Routes></div><Footer /><LeadChat /></div></BrowserRouter></TooltipProvider></QueryClientProvider>;
+const App = () => <QueryClientProvider client={queryClient}><TooltipProvider><Toaster /><Sonner /><BrowserRouter><ScrollToTop /><div className="flex min-h-screen flex-col"><Header /><div className="flex-1"><Routes><Route path="/" element={<Index />} /><Route path="/sobre" element={<AboutPage />} /><Route path="/solucoes/:slug" element={<DetailPage />} /><Route path="/treinamentos/:slug" element={<DetailPage />} /><Route path="/blog" element={<ArchivePage kind="blog" />} /><Route path="/conhecimentos" element={<ArchivePage kind="conhecimento" />} /><Route path="/cases" element={<ArchivePage kind="case" />} /><Route path="/glossario" element={<GlossaryPage />} /><Route path="/glossario/:slug" element={<GlossaryTermPage />} /><Route path="/:kind/:slug" element={<ArticlePage />} /><Route path="*" element={<NotFound />} /></Routes></div><Footer /><LeadChat /></div></BrowserRouter></TooltipProvider></QueryClientProvider>;
 
 export default App;
